edbd828bbd215e4dde34fdbe0308eac5c91bb117
Gregory Haydon <110743586+ghaydon30@users.noreply.github.com>
2025-06-07 21:46:52 -0500

    Fixed chat formatting issue.

--- components/Chat.js
+++ components/Chat.js
@@ -88,8 +88,8 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
 
